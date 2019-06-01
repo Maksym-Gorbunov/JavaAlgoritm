4aa5e2d792d09cd0fdca2a408467c240894da04f
Maksym-Gorbunov <maxsverige@hotmail.com>
2019-06-01 13:50:07 +0200

    quickSort

--- src/Algoritm.java
+++ src/Algoritm.java
@@ -1,10 +1,18 @@
 import java.util.Arrays;
 
 public class Algoritm {
+
+    public static int[] array = {6,3,7,2,164};
+
     public static void main(String[] args) {
         System.out.println(divide(17, 4));
         int[] arr = {6,3,7,2,14};
         bubbleSort(arr);
+//        quickSort();
+        int [] b = Arrays.copyOf(array, array.length);
+        Arrays.sort(array);
+        System.out.println(Arrays.toString(b));
+//        System.out.println(Arrays.sort(array));
     }
 
     // Eucaliptov, obschij delitel
@@ -26,5 +34,36 @@ public class Algoritm {
             }
         }
         System.out.println(Arrays.toString(arr));
+    }
+
+    public static void quickSort(){
+        int start = 0;
+        int end = array.length-1;
+        doSort(start, end);
+    }
+    private static void doSort(int start, int end){
+        if(start >= end) return;
+        int i = start, j = end;
+        int cur = i - (i-j)/2;
+        while(i<j){
+            while((i < cur) && (array[i] <= array[cur])){
+                i++;
+            }
+            while((cur < j) && (array[cur] <= array[j])){
+                j--;
+            }
+            if(i<j){
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+                if(i == cur){
+                    cur = j;
+                } else if(j == cur){
+                    cur = i;
+                }
+            }
+        }
+        doSort(start, cur);
+        doSort(cur+1, end);
     }
 }
